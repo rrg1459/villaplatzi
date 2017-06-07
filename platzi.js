e2341822e801c7157f56bc2f5562ea65135a6209
Rafael <rrg1459@hotmail.com>
2017-06-07 14:53:40 -0400

    segundo commit

--- platzi.js
+++ platzi.js
@@ -1,15 +1,81 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext('2d');
-var mapa = "tile.png";
 
-var image = new Image();
-imagen.src = mapa;
-imagen.addEventListener("load", dibujar);
+
+var fondo = {
+	url: "tile.png",
+	cargaOK: false
+};
+
+var vaca = {
+	url: "vaca.png",
+	cargaOK: false
+};
+
+var cerdo = {
+	url: "cerdo.png",
+	cargaOK: false
+};
+
+var pollo = {
+	url: "pollo.png",
+	cargaOK: false
+};
+
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
+
+
+function cargarFondo()
+{
+	fondo.cargaOK = true;
+	dibujar();
+}
+
+function cargarVacas()
+{
+	vaca.cargaOK = true;
+	dibujar();
+}
+
+function cargarCerdos()
+{
+	cerdo.cargaOK = true;
+	dibujar();
+}
+
+function cargarPollos()
+{
+	pollo.cargaOK = true;
+	dibujar();
+}
 
 function dibujar()
 {
-	papel.drawImage
+	if (fondo.cargaOK) 
+	{
+		papel.drawImage(fondo.imagen, 0 , 0);
+	}
+	if (vaca.cargaOK) 
+	{
+		papel.drawImage(vaca.imagen, 100 , 100);
+	}
 }
+
 
 
 function aleatorio (min, maxi)
